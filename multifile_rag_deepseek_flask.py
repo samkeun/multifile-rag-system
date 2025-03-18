8e796f6b16a8344eb869d5b29c1744a7a0f8bfe8
samkeun <skimok@gmail.com>
2025-03-18 19:46:26 +0900

    Add files via upload

--- multifile_rag_deepseek_flask.py
+++ multifile_rag_deepseek_flask.py
@@ -116,5 +116,9 @@ def query_rag():
     return jsonify({"response": response})
 
 
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000, debug=True)
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=False)
+
